f60cb029a3da06b517d663e7b91f1ff0467ba332
Juncy <jyunkeeees724@gmail.com>
2020-07-20 03:18:43 +0900

    [タイトル] ArrowSever
    [ジャンル]
    アクション
    
    [バージョン]
    Unity 2019.2.17f1
    
    [プラットフォーム]
    IOS向け-iPhone8 or 7を想定して作成。

---
+++ ArrowSever/Assets/Script/UI/Clear_Time.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Clear_Time : MonoBehaviour
+{
+    
+    int BlockScore;
+    int ComboScore;
+    int Score;
+    public int Block_odds = 100;
+    public int MaxCombo_odds = 100;
+
+
+
+    GameObject Combo_result;
+    GameObject Score_result;
+    GameObject Block_result;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // this.Time_result = GameObject.Find("Time_Result");
+        this.Block_result = GameObject.Find("Block_Result");
+        this.Combo_result = GameObject.Find("Combo_Result");
+        this.Score_result = GameObject.Find("Score_Result");
+
+        // UI_ScriptのSecond,Comboの値を参照
+        BlockScore = UI_Script.Combo;
+        ComboScore = UI_Script.MaxCombo;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.Block_result.GetComponent<Text>().text = "個数: " + UI_Script.Combo.ToString("D2");
+        this.Combo_result.GetComponent<Text>().text = "COMBO: " + UI_Script.MaxCombo.ToString("D2");
+
+        Score = (BlockScore * Block_odds) + (ComboScore * MaxCombo_odds);
+        this.Score_result.GetComponent<Text>().text = "SCORE: " + Score.ToString("D4");
+
+    }
+}
---
+++ ArrowSever/Assets/Script/UI/Start_Botton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Start_Botton : MonoBehaviour
+{
+    public void First_Botton()
+    {
+        UI_Script.Combo = 0;
+        UI_Script.Max_Break_Block = 0;
+        UI_Script.MaxCombo = 0;
+        Collision_L_Sword.Combo_Counter = 0;
+        Collision_R_Sword.Combo_Counter = 0;
+
+        SceneManager.LoadScene("MainScene");
+        
+    }
+}
